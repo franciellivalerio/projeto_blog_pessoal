f41d9f9a019583b2f76a89554e129b85879d4b0b
franciellivalerio <franciellivaleriodeoliveira@gmail.com>
2024-07-23 13:10:50 -0300

    CRUD da postagem finalizado

---
+++ src/postagem/controllers/postagem.controller.ts
@@ -0,0 +1,46 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { PostagemService } from "../services/postagem.service";
+import { Postagem } from "../entities/postagem.entity";
+
+@Controller("/postagens")
+export class PostagemController{
+
+    constructor(private readonly postagemService: PostagemService){}
+
+    @Get()
+    @HttpCode(HttpStatus.OK) // Http Status 200
+    findAll(): Promise<Postagem[]>{
+        return this.postagemService.findAll();
+    }
+    
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK) // Http Status 200
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem>{
+        return this.postagemService.findById(id);
+    }
+
+    @Get('/titulo/:titulo')
+    @HttpCode(HttpStatus.OK) // Http Status 200
+    findByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]>{
+        return this.postagemService.findByTitulo(titulo);
+    }
+    
+    @Post()
+    @HttpCode(HttpStatus.CREATED) // Http Status 201
+    create(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.create(postagem);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK) // Http Status 200
+    update(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.update(postagem);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT) // Http Status 204
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.postagemService.delete(id);
+    }
+
+}
---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,12 @@
+import { Module } from "@nestjs/common";
+import { Postagem } from "./entities/postagem.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Postagem])],
+    providers: [],
+    controllers: [],
+    exports: [TypeOrmModule]
+})
+
+export class PostagemModule { }
